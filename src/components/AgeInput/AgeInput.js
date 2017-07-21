912353212dd7be0cb0315a23ad5cdbff79c4579b
jaotors <turingan.joshua@gmail.com>
2017-07-21 19:17:54 +0800

    remove compenentDidMount

--- src/components/AgeInput/AgeInput.js
+++ src/components/AgeInput/AgeInput.js
@@ -33,21 +33,6 @@ class AgeInput extends React.Component {
     })
   }
 
-  componentDidMount() {
-    let errors = []
-    if(!errors.some(err => err === this.state.errCodes[0])) errors = errors.concat(this.state.errCodes[0])
-    if(this.props.coverSelect === 3) {
-      if(!errors.some(err => err === this.state.errCodes[2])) errors = errors.concat(this.state.errCodes[2])
-    }
-    if(this.props.coverSelect === 3 || this.props.coverSelect === 4) {
-      if(!errors.some(err => err === this.state.errCodes[4])) errors = errors.concat(this.state.errCodes[4])
-    }
-
-    this.setState({
-      errors: errors
-    })
-  }
-
   handleChange(e) {
     let errors = this.state.errors
     const {id, value} = e.target
